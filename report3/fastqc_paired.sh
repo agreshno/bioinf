36b72a7763756bc065aec5ec4a5108b8ded7b055
Aleksandra Greshnova <greshnova.aleksandra@gmail.com>
2020-04-06 18:36:57 +0200

    Create fastqc_paired.sh

---
+++ report3/fastqc_paired.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+#
+
+#we first start by giving the cluster a few options on the job: which queue to put it into (nick), and the name we are giving it
+
+#SBATCH --partition=defaultp
+
+#SBATCH --job-name=fastqc
+
+#SBATCH --output=fastqc_output
+
+#
+
+#Number of CPU cores to use within one node
+
+#SBATCH -c 5
+
+#
+
+#Define the number of hours the job should run.
+
+#Maximum runtime is limited to 10 days, ie. 240 hours
+
+#SBATCH --time=96:00:00
+
+#
+
+#Define the amount of RAM used by your job in GigaBytes
+
+#In shared memory applications this is shared among multiple CPUs
+
+#SBATCH --mem=10G
+
+#
+
+#Send emails when a job starts, it is finished or it exits
+
+#SBATCH --mail-user=agreshno@ist.ac.at
+
+#SBATCH --mail-type=ALL
+
+#
+
+#Pick whether you prefer requeue or not. If you use the --requeue
+
+#option, the requeued job script will start from the beginning,
+
+#potentially overwriting your previous progress, so be careful.
+
+#For some people the --requeue option might be desired if their
+
+#application will continue from the last state.
+
+#Do not requeue the job in the case it fails.
+
+#SBATCH --no-requeue
+
+#
+
+#Do not export the local environment to the compute nodes
+
+#SBATCH --export=NONE
+
+unset SLURM_EXPORT_ENV
+
+#
+
+#Set the number of threads to the SLURM internal variable
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+#
+
+#load any module you need here
+
+module load java
+module load fastqc
+
+#run commands on SLURM's srun
+
+srun fastqc ERR305392_1_paired.fastq 
+
+srun fastqc ERR305392_2_paired.fastq
