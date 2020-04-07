8ae0103b05a225e3ecfd0a44b63128565104e055
Aleksandra Greshnova <greshnova.aleksandra@gmail.com>
2020-04-07 19:58:36 +0200

    Update rm_mult_map.sh

--- report3/rm_mult_map.sh
+++ report3/rm_mult_map.sh
@@ -75,10 +75,10 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 #run commands on SLURM's srun
 
-srun grep -v 'XS:i:' SRR859218.sam > SRR859218.sam
+srun grep -v 'XS:i:' SRR859218.sam > SRR859218_uniq.sam
 
-srun grep -v 'XS:i:' SRR859219.sam > SRR859219.sam
+srun grep -v 'XS:i:' SRR859219.sam > SRR859219_uniq.sam
 
-srun grep -v 'XS:i:' SRR859220.sam > SRR859220.sam
+srun grep -v 'XS:i:' SRR859220.sam > SRR859220_uniq.sam
 
-srun grep -v 'XS:i:' SRR859221.sam > SRR859221.sam
+srun grep -v 'XS:i:' SRR859221.sam > SRR859221_uniq.sam
